4fdd121fd3396873be281c46fb4d147199c304d4
Exi <excel.ekwenuya@gmail.com>
2024-11-06 13:01:13 +0000

    designed admin dashboard

---
+++ README.md
@@ -0,0 +1,15 @@
+# Task 2
+This is my school project. I will build a fully working website. I will build an E-Commerce application.
+
+## Features
+- Admin Features
+    - Create Products
+
+## Routes
+- Admin Routes
+    - `/admin`: Admin Dashboard
+
+## Running The Application
+```sh
+php -S localhost:8000
+```
---
+++ admin/index.php
@@ -0,0 +1,60 @@
+<?php
+
+    include_once "../includes/header.php"
+
+?>
+
+    <div class="container py-5">
+        <div class="row">
+            <h1 class="text-center">Welcome to the admin dashboard</h1>
+            <div class="col-6">
+                <form action="" method="POST">
+                    <h2>Add New Product</h2>
+                    <div class="mb-3">
+                        <label for="productName" class="form-label">Product name</label>
+                        <input type="text" class="form-control" id="productName" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="quantity" class="form-label">Product Quantity</label>
+                        <input type="number" name="quantity" id="quantity" placeholder="Count" class="form-control" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="image" class="form-label">Product Image</label>
+                        <input type="file" name="image" id="image" class="form-control">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Add Product</button>
+                </form>
+            </div>
+            <div class="col-6">
+                <h2>Products List</h2>
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <td>Name</td>
+                            <td>Quantity Left</td>
+                            <td>Edit</td>
+                            <td>Delete</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Large Kuli Kuli</td>
+                            <td>14</td>
+                            <td>
+                                <button class="btn btn-warning btn-sm">Edit</button>
+                            </td>
+                            <td>
+                                <button class="btn btn-danger btn-sm">Delete</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+<?php
+
+    include_once "../includes/footer.php"
+
+?>
